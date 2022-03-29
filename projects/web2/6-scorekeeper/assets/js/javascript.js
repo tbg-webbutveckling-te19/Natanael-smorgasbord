7434c418e3d881968987d93c635827e94dbc0fde
Natanael Laska <colagubb3n.hotmail.com>
2022-03-29 12:52:36 +0200

    version 2 scorekeeper

--- projects/web2/6-scorekeeper/assets/js/javascript.js
+++ projects/web2/6-scorekeeper/assets/js/javascript.js
@@ -1,5 +1,6 @@
 
 scoreGoal=-1
+hasWon=false
 
 var player1 = document.querySelector("#isClickedP1")
 
@@ -19,9 +20,12 @@ var resetBtn = document.querySelector("#resetBtn")
 
 inputScoreGoal.addEventListener("keydown", function(e){
     if (e.key==='Enter'){
-        scoreGoal=inputScoreGoal.value
-        playingTo.innerHTML=(`Playing to ${scoreGoal}`)
-        inputScoreGoal.value=""
+        
+        if (isNaN(inputScoreGoal.value)==false){
+            scoreGoal=inputScoreGoal.value
+            playingTo.innerHTML=(`Playing to ${scoreGoal}`)
+            inputScoreGoal.value=""
+        }
     }
 })
 
@@ -31,37 +35,43 @@ var scoreX=0
 
 
 player1.addEventListener("click", function(){
-    score1+=1
-    score.innerHTML=(`Score: ${score1} - ${score2}`)
-    if (score1>scoreGoal){
-        score1=scoreX
-    }
-    if (score1 == scoreGoal){
-        result.innerHTML=("Player 1 Won")
-        score.innerHTML=(`Score: <span style="color: Green">${score1}</span> - <span style="color: Red">${score2}</span)`)
-        scoreX=score1
-    }
-    
-    if (scoreGoal ==-1){
-        score.innerHTML=(`Score: 0-0`)
-        result.innerHTML=""
+    if (hasWon==false){
+        score1+=1
+        score.innerHTML=(`Score: ${score1} - ${score2}`)
+        if (score1>scoreGoal){
+            score1=scoreX
+        }
+        if (score1 == scoreGoal){
+            result.innerHTML=("Player 1 Won")
+            score.innerHTML=(`Score: <span style="color: Green">${score1}</span> - <span style="color: Red">${score2}</span)`)
+            scoreX=score1
+            hasWon=true
+        }
+        
+        if (scoreGoal ==-1){
+            score.innerHTML=(`Score: 0-0`)
+            result.innerHTML=""
+        }
     }
 })
 
 player2.addEventListener("click", function(){
-    score2+=1
-    score.innerHTML=(`Score: ${score1} - ${score2}`)
-    if (score2>scoreGoal){
-        score2=scoreX
-    }
-    if (score2 == scoreGoal || score2>scoreGoal){
-        result.innerHTML=("Player 2 Won!")
-        score.innerHTML=(`Score: <span style="color: Red">${score1}</span> - <span style="color: Green">${score2}</span)`)
-        scoreX=score2
-    }
-    if (scoreGoal ==-1){
-        score.innerHTML=(`Score: 0-0`)
-        result.innerHTML=""
+    if (hasWon==false){
+        score2+=1
+        score.innerHTML=(`Score: ${score1} - ${score2}`)
+        if (score2>scoreGoal){
+            score2=scoreX
+        }
+        if (score2 == scoreGoal || score2>scoreGoal){
+            result.innerHTML=("Player 2 Won!")
+            score.innerHTML=(`Score: <span style="color: Red">${score1}</span> - <span style="color: Green">${score2}</span)`)
+            scoreX=score2
+            hasWon=true
+        }
+        if (scoreGoal ==-1){
+            score.innerHTML=(`Score: 0-0`)
+            result.innerHTML=""
+        }
     }
 })
 
